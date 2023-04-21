49d873f2fd96457de94ada40463257f3df385db7
Vicky-E <victoriaemmanuel630@gmail.com>
2023-04-20 20:08:01 -0700

    print

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Sucess)
+ */
+int main(void)
+{char prop_gram="with proper grammar, but the outcome is a piece of art,";
+printf("%s\n",prop_gram;
+	return (0)
+}
+
+
+
