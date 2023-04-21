2dab23b9d1bd1151017034e69bcdbd961f82a064
Vicky-E <victoriaemmanuel630@gmail.com>
2023-04-20 20:34:24 -0700

    pting

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -5,8 +5,9 @@
  * Return: Always 0 (Sucess)
  */
 int main(void)
-{char prop_gram= "with proper grammar, but the outcome is a piece of art,";
-printf ("%s\n",prop_gram);
+{char prop_gram[]= "with proper grammar, but the outcome is a piece of art,";
+fgets(prop_gram,40, stdin);
+printf ("%s",prop_gram);
 	return (0);
 }
 
