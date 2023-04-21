067daa88505bd8d967c324030daa11f8750c427b
Vicky-E <victoriaemmanuel630@gmail.com>
2023-04-20 20:55:28 -0700

    fixing

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -5,9 +5,9 @@
  * Return: Always 0 (Sucess)
  */
 int main(void)
-{char prop_gram[]= "with proper grammar, but the outcome is a piece of art,";
-fgets(prop_gram,40, stdin);
-printf("%s\n" , prop_gram);
+{char prop_gram[] = "with proper grammar, but the outcome is a piece of art,";
+fgets(prop_gram, 40, stdin);
+printf("%s\n", prop_gram);
 	return (0);
 }
 
