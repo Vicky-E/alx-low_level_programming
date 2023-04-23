72def3020bac3ef7639e2af9b9e2b5c51f584373
Vicky-E <victoriaemmanuel630@gmail.com>
2023-04-23 10:19:29 -0700

    size

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -4,8 +4,13 @@
  *
  * Return: Always 0 (Sucess)
  */
+#define Data_size(type) printf("size of a" #type: "%zu\n",sizeof(type))
 int main(void)
 {
-printf("size of a" (Type): "%zu\n" sizeof(Type));
+Data_size(char);
+Data_size(int);
+Data_size(long int);
+Data_size(long long int);
+Data_size(float);
 	return (0);
 }
