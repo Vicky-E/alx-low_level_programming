86e328e7ff4b36aede7c0e9f52a223139fb88ecf
Vicky-E <victoriaemmanuel630@gmail.com>
2023-04-24 10:20:42 -0700

    quote

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -2,9 +2,10 @@
 /**
  * main - Entry point
  *
- * Return: Always 1
+ * Return: Always 1 (Sucess)
  */
+#define RETURN return (1)
 int main(void)
 {fprintf(stderr, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n");
-return (1);
+RETURN;
 }
