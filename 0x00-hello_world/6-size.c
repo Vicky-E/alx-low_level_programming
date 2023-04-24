3ba5bacf75240278a9b83064fee3c62a6bef58c3
Vicky-E <victoriaemmanuel630@gmail.com>
2023-04-24 06:27:24 -0700

    size again

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -10,10 +10,10 @@ int int_size;
 long int long_int;
 long long int long_2int;
 float float_size;
-printf("size of a char: %zu byte(s)\n", sizeof(char_size));
-printf("size of an int: %zu byte(s)\n", sizeof(int_size));
-printf("size of a long int: %zu byte(s)\n", sizeof(long_int));
-printf("size of a long long int: %zu byte(s)\n", sizeof(long_2int));
-printf("size of a float: %zu byte(s)\n", sizeof(float_size));
+printf("size of a char: %lu byte(s)\n", sizeof(char_size));
+printf("size of an int: %lu byte(s)\n", sizeof(int_size));
+printf("size of a long int: %lu byte(s)\n", sizeof(long_int));
+printf("size of a long long int: %lu byte(s)\n", sizeof(long_2int));
+printf("size of a float: %lu byte(s)\n", sizeof(float_size));
 	return (0);
 }
