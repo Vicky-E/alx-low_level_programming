17725dfb061b45440c4ade96d10993969aef9377
Vicky-E <victoriaemmanuel630@gmail.com>
2023-04-23 18:10:48 -0700

    data size

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -4,15 +4,17 @@
  *
  * Return: Always 0 (Sucess)
  */
-void Data_size(type){
-       	printf("size of a" #type ":%zu\n",sizeof(type));
-}
 int main(void)
 {
-Data_size(char);
-Data_size(int);
-Data_size(long int);
-Data_size(long long int);
-Data_size(float);
+char char_size;
+int int_size;
+long int long_int;
+long long int long_2int;
+float float_size;
+printf("size of a  char: %zu byte(s)\n",sizeof(char_size));
+printf("size of an int: %zu byte(s)\n",sizeof(int_size));
+printf("size of a long int: %zu byte(s)\n",sizeof(long_int));
+printf("size of a long long int: %zu byte(s)\n",sizeof(long_2int));
+printf("size of a float: %zu byte(s)\n",sizeof(float_size)); 
 	return (0);
 }
