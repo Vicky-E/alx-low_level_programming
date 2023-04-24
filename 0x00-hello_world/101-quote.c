bcec9aab4178d37df5d2128e2ec11ae9cf610c7c
Vicky-E <victoriaemmanuel630@gmail.com>
2023-04-24 10:54:27 -0700

    again

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -6,7 +6,6 @@
  */
 int main(void)
 {
-char strings= "and that piece of art is useful\" - Dora Korpar, 2015-10-19";
-fprintf(stderr, strings "\n"); 
+fprintf(stderr, "and that piece of art is useful\" - Dora Korpar, 2015-10-1\n"); 
 return (1);
 }
