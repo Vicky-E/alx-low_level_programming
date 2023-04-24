cd542818f6b8c333b86d662730e88c63e8fd3001
Vicky-E <victoriaemmanuel630@gmail.com>
2023-04-24 09:44:05 -0700

    quotes

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 1 (Sucess)
+ */
+int main(void)
+{fprintf(stderr,"and that piece of art is useful\" - Dora Korpar, 2015-10-19 %s\n");
+ return (1);
+}
