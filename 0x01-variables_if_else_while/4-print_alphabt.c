5cf3fa67da121e54b3b984d627e41c6382b541cb
Vicky-E <victoriaemmanuel630@gmail.com>
2023-04-24 21:49:13 -0700

    print

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -7,13 +7,11 @@
 int main(void)
 {char x;
 for (x = 'a'; x <= 'z'; x++)
-
-{if (x == 'q' || x == 'e')
 	{
-            continue;
-	}
+		if (x == 'q' || x == 'e')
+			continue;
 	putchar(x);
-}
+	}
 putchar('\n');
 return (0);
 }
