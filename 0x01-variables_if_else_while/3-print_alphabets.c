18ee0645c6ab0ee3190bf5ec72dd7e2c5031d5f1
Vicky-E <victoriaemmanuel630@gmail.com>
2023-04-24 21:11:39 -0700

    upper and lower

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Sucess)
+ */
+int main(void)
+{char x;
+char X;	
+for (x = 'a'; x <= 'z'; x++)
+putchar(x);
+for (X = 'A'; X <= 'Z'; X++)
+putchar(X)
+putchar('\n');
+return (0);
+}
