c27a179df5d06e0f4f0c4616412665d7d5db0e2c
Vicky-E <victoriaemmanuel630@gmail.com>
2023-04-25 11:57:16 -0700

    positives

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -1,6 +1,11 @@
 #include <stdlib.h>
 #include <time.h>
 #include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Sucess)
+ */
 
 /* betty style doc for function main goes there */
 int main(void)
