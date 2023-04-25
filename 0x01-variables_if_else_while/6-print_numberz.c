8ab1d1a5c88fc7f5af95574ba153e974c4bb485c
Vicky-E <victoriaemmanuel630@gmail.com>
2023-04-24 22:44:20 -0700

    numberz

--- 0x01-variables_if_else_while/6-print_numberz.c
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -1,11 +1,12 @@
 #include <stdio.h>
+#include <stdlib.h>
 /**
  * main - Entry point
  *
  * Return: Always 0 (Sucess)
  */
 int main(void)
-{unsigned int  num;
+{int  num;
 for (num = 0; num <= 9; num++)
 putchar(num);
 putchar('\n');
