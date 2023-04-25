354ba47b6414ee77fd7e0271a028bd120a10ebd4
Vicky-E <victoriaemmanuel630@gmail.com>
2023-04-25 08:51:31 -0700

    HEX

--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -6,10 +6,10 @@
  */
 int main(void)
 {int a;
-int hex[] = {0,1,2,3,4,5,6,7,8,9};
+int hex[] = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9};
 char x;
 for (a = 0; a <= 9; a++)
-putchar( '0' + hex[a]);
+putchar('0' + hex[a]);
 for (x = 'a'; x <= 'f'; x++)
 putchar(x);
 
