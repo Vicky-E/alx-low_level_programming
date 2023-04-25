1868d3198b5f204d88810156d22b35d8a725600a
Vicky-E <victoriaemmanuel630@gmail.com>
2023-04-24 21:01:31 -0700

    putchar

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -1,15 +1,13 @@
 #include <stdio.h>
 /**
- *
  * main - Entry point
  *
  * return always 0 (sucess)
  */
 int main(void)
 {char x;
-for (x= 'a';x <='z';x++){
-		putchar(x);
-	}
+for (x = 'a';x <= 'z';x++)
+putchar(x);
 putchar('\n');
 return (0);
 }
