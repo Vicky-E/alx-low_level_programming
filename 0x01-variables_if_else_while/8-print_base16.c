9a7b3da07cc07472fea8af36dd07215926209fd3
Vicky-E <victoriaemmanuel630@gmail.com>
2023-04-25 06:31:29 -0700

    Hexadecimal

--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -5,9 +5,12 @@
  * Return: Always 0 (Sucess)
  */
 int main(void)
-{char a;
+{int a;
 for (a = 0; a <= 15; a++)
-	putchar('%x',a);
+{
+	putchar('%x');
+	putchar(a);
+}
 putchar('\n');
 return (0);
 }
