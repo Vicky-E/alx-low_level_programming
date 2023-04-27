65a89e2a4b4b43856eb21bf228038805d87c4dbd
Vicky-E <victoriaemmanuel630@gmail.com>
2023-04-26 21:56:03 -0700

    char

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,13 +1,13 @@
 #include <unistd.h>
-
+#include "main.h"
 /**
  * pchar - prints _putchar
  *
  * Return: Always 0 (Sucess)
  */
-int pchar()
+void  pchar()
 {	char a;
 	a = "_putchar";
 	_putchar(a);
-	return (0);
+	
 }
