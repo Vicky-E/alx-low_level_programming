7c0ae1b6cb5b63ae5d34eb912166e17ee7ceb0e0
Vicky-E <victoriaemmanuel630@gmail.com>
2023-04-26 20:03:36 -0700

    0-putchar

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,13 @@
+#include <unistd.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Sucess)
+ */
+int main(void)
+{	char a = "_putchar";
+	putchar(a);
+	putchar('\n');
+	return (0);
+}
---
+++ 0x02-functions_nested_loops/README.md
@@ -0,0 +1,1 @@
+Nested loops,function prototypes and definitions
