70c7bb7aa2721f2f100974b36eb708b297b12d8d
Vicky-E <victoriaemmanuel630@gmail.com>
2023-04-27 16:35:01 -0700

    signs

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,23 @@
+#include <unistd.h>
+#include "main.h"
+/**
+ * print_sign - checks for sign of numbe
+ * @n:character to print
+ *
+ * Return: On sucess 1
+ */
+int print_sign(int n)
+{
+	if (n > 0)
+		return (1);
+		_putchar('+');
+	else if (n == 0)
+		return (0);
+		_putchar(n);
+	else if (n < 0)
+		return (-1);
+		_putchar('-');
+	else
+		_putchar(n);
+	return (0);
+}
