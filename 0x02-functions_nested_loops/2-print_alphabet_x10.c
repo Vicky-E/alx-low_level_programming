2b99c8e0a5c537250f194d12a0cf5abdec83370a
Vicky-E <victoriaemmanuel630@gmail.com>
2023-04-26 22:35:01 -0700

    multiples

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -6,13 +6,17 @@
  * Return: always 0 (Sucess)
  */
 void print_alphabet_x10(void)
-{       int a;
+{	int a;
 	int b;
 	b = 0;
-	while (b < 11){
-        for (a = 'a'; a <= 'z'; a++)
-        _putchar(a);
-	_putchar('\n');
-	b++;
+	while (b < 10)
+	{
+		for (a = 'a'; a <= 'z'; a++)
+			{
+				_putchar(a);
+				_putchar('\n');
+			}
+		b++;
+			
 	}
 }
