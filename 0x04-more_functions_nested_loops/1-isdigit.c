009f7fb7214d9912b6f8d0b76e35fb439b263b24
Vicky-E <victoriaemmanuel630@gmail.com>
2023-04-28 08:39:04 -0700

    digits

---
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,13 @@
+#include <unistd.h>
+#include "main.h"
+/**
+ * _isdigit - checks for digit
+ * @c: output
+ * Returns: c
+ */
+int _isdigit(int c)
+{	if (c > = 0  || c < 0)
+ 		return (1);
+	else
+		return (0);
+}
