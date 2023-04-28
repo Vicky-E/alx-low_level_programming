6acada945d023b4e91f8c6e1db563e988d3f37fd
Vicky-E <victoriaemmanuel630@gmail.com>
2023-04-28 07:30:59 -0700

    last

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -14,9 +14,9 @@ int print_last_digit(int x)
 	if(x < 0)
 	{
 		x = (-1 * x);
-		return(write(1, &x, x));
+		return(write(1, &x, &x));
 	}
 	else
-		return(write(1, &x, x));
+		return(write(1, &x, &x));
 }
 	
