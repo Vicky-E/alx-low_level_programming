7c069093495e11590313ce2c61f99ad711820889
Vicky-E <victoriaemmanuel630@gmail.com>
2023-04-29 07:06:13 -0700

    times

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -5,18 +5,19 @@
  *
  * Return: 0
  */
-void times_table()
+void times_table(void)
 {	int x = 0;
 
 	int z;
 	int y;
 	for (y = 0; y < 10; y++)
-	{
-		while (x < 10)
+	{	
+		for (x = 0; x < 10; x++)
 		{
 			z = x * y;
 			_putchar('0' + z);
-			x++;
+			_putchar(',');
+			_putchar(' ');
 		}
 		_putchar('\n');
 	}
