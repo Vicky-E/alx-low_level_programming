c2e6e7e85c8fcb3d2ea729bb52aef48f4d03cb71
Vicky-E <victoriaemmanuel630@gmail.com>
2023-04-29 04:23:14 -0700

    100

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: On sucess 0
+ */
+int main(void)
+{int x = 0;
+for (y = 0; y <= 9; y++)
+{	
+        while (x <= 9)
+        {
+                putchar('0' + x);
+		putchar('0' + y)
+                putchar(',');
+                putchar(' ');
+
+                x++;
+		if (x == y)
+			continue;
+        }
+        putchar('\n');
+        return (0);
+}
