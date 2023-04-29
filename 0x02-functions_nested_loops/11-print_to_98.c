e272d69e46cba1b48d2980f596763a59a5712840
Vicky-E <victoriaemmanuel630@gmail.com>
2023-04-29 09:31:54 -0700

    prints 98

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -3,7 +3,7 @@
 #include <unistd.h>
 /**
  * print_to_98 - prints natural numbers to 98
- *
+ * @n: number to be printed
  */
 void print_to_98(int n)
 {	while (n < 98)
@@ -23,5 +23,4 @@ void print_to_98(int n)
 	}
 	if (n == 98)
 		printf("%d\n", n);
-
-}	       	
+}
