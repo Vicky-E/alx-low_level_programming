c3c396c7d167123d582a4a9f8e78028162b54dc7
Vicky-E <victoriaemmanuel630@gmail.com>
2023-04-29 09:35:08 -0700

    98

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -4,15 +4,16 @@
 /**
  * print_to_98 - prints natural numbers to 98
  * @n: number to be printed
+ *
  */
 void print_to_98(int n)
-{	while (n < 98)
+{	
+	while (n < 98)
 	{
 		printf("%d", n);
 		putchar(',');
 		putchar(' ');
 		n++;
-		
 	}
 	while (n > 98)
 	{
