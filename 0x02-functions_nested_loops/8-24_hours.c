25db1429940dc0094c5caf7ca6d7072f541ea402
Vicky-E <victoriaemmanuel630@gmail.com>
2023-04-30 09:35:14 -0700

    read

---
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -0,0 +1,8 @@
+#include <unistd.h>
+#include "main.h"
+/**
+ * jack_bauer _ prints every hour and minute of the day
+ *
+ */
+void jack_bauer(void)
+{
---
+++ README.md
@@ -0,0 +1,2 @@
+Basic c programming
+debugging code
