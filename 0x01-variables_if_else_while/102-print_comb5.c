54ae8ccb59a4250183f25dcc468171edf2e738dd
Vicky-E <victoriaemmanuel630@gmail.com>
2023-05-01 12:02:41 -0700

    102

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -22,13 +22,13 @@ int main(void)
 					putchar(w + '0');
 					putchar(x + '0');
 					putchar(' ');
-					putchar(y + '0');
+					/**putchar(y + '0');
 					putchar(z + '0');
 					if ((w == 9 && x == 8) && (y == 9 && z == 9))
 							continue;
 					putchar(',');
 					putchar(' ');
-				}
+				}*/
 			}
 			x++;
 		}
