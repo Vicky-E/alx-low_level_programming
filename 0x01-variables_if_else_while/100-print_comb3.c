c9700e5fac5f1650b56055cdb2d551688fd783b5
Vicky-E <victoriaemmanuel630@gmail.com>
2023-05-01 10:04:50 -0700

    comb

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -15,16 +15,16 @@ while (y <= 9)
 	for(x = y + 1; x <= 9; x++)
 	{	if (x == y)
 			continue;
-		putchar('0' + (y % 10));
-		putchar('0' + (x % 10));
+		putchar('0' + y);
+		putchar('0' + x);
 		if (y == 8 && x == 9)
 			continue;
 		putchar(',');
 		putchar(' ');
-	
+
 	}
 	y++;
 }
-
+	putchar('\n');
 	return (0);
 }
