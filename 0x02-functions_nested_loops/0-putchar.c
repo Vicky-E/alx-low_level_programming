6285830b1bb9f591f626b3e3eb411a938225997f
Vicky-E <victoriaemmanuel630@gmail.com>
2023-05-01 02:04:23 -0700

    char

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -6,15 +6,12 @@
  * Return: Always 0 (Sucess)
  */
 int  pchar()
-{	char p, u, t, c, h, a, r;
+{	int p, u;
+	p = "put";
+	u = "char";
 	_putchar('_');
 	_putchar(p);
 	_putchar(u);
-	_putchar(t);
-	_putchar(c);
-	_putchar(h);
-	_putchar(a);
-	_putchar(r);
 	_putchar('\n');
 	return (0);
 }
