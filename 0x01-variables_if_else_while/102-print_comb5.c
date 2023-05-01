e83cff8912e501c5693ea05596beb11140173d2c
Vicky-E <victoriaemmanuel630@gmail.com>
2023-05-01 11:35:58 -0700

    102

---
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -0,0 +1,38 @@
+#include <stdio.h>
+/**
+ * main - prints two two digits numbers
+ *
+ * Return: 0
+ */
+int main(void)
+{
+	int w, x, y, z;
+	w = 0;
+	x = 0;
+	y = 0;
+
+	while (w < 10)
+	{
+		while (x < 10)
+		{
+			while (y < 10)
+			{
+				for (z = y + 1; z < 10; z++)
+				{
+					putchar(w + '0');
+					putchar(x + '0');
+					putchar(' ');
+					putchar(y + '0');
+					putchar(z + '0');
+					putchar(',');
+					putchar(' ');
+				}
+				y++;
+			}
+			x++;
+		}
+		w++;
+	}
+	return (0);
+}
+
