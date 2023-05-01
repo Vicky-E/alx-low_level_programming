4061a13a87157ba605fd2704bfd03a8cb8a4921b
Vicky-E <victoriaemmanuel630@gmail.com>
2023-05-01 12:08:22 -0700

    p

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -11,7 +11,7 @@ int main(void)
 	w = 0;
 	x = 0;
 
-	while (w < 10)
+	/**while (w < 10)
 	{
 		while (x < 10)
 		{
@@ -22,18 +22,18 @@ int main(void)
 					putchar(w + '0');
 					putchar(x + '0');
 					putchar(' ');
-					/**putchar(y + '0');
+					putchar(y + '0');
 					putchar(z + '0');
 					if ((w == 9 && x == 8) && (y == 9 && z == 9))
 							continue;
 					putchar(',');
-					putchar(' ');*/
+					putchar(' ');
 				}
 			}
 			x++;
 		}
 		w++;
-	}
+	}*/
 	putchar('\n');
 	return (0);
 }
