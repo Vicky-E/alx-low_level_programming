82ed9dd7c44d08bb395b9167d6a818d045d0274a
Vicky-E <victoriaemmanuel630@gmail.com>
2023-05-01 10:41:25 -0700

    comb4

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -13,15 +13,15 @@ int z;
 
 while (y <= 9)
 {
-	for (z = x + 1; z <= 9; z++)
+	for (z = y + 1; z <= 9; z++)
 	{
-		for (x = y + 1; x <= 9; x++)
+		for (x = z + 1; x <= 9; x++)
 		{
                 	if (x == y && y == z)
 				continue;
 			putchar('0' + y);
-			putchar('0' + x);
 			putchar('0' + z);
+			putchar('0' + x);
 			if (y == 7 && x == 8 && z == 9)
 				continue;
 			putchar(',');
