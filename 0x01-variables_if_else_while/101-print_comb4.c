4fc32027c8fd149082dacd88d78e96d8211e1b70
Vicky-E <victoriaemmanuel630@gmail.com>
2023-05-01 10:26:16 -0700

    comb4

---
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: On sucess 0
+ */
+int main(void)
+{
+int y = 0;
+
+int x;
+int z;
+
+while (y <= 9)
+{
+	for (z = x + 1, z <= 9; z++)
+	{
+		for (x = y + 1; x <= 9; x++)
+		{
+                	if (x == y == z)
+				continue;
+			putchar('0' + y);
+			putchar('0' + x);
+			putchar('0' + z);
+			if (y == 8 && x == 9)
+				continue;
+			putchar(',');
+			putchar(' ');
+
+		}
+		z++;
+	}
+	y++
+}
+	putchar('\n');
+	return (0);
+}
