d6fb096464a2664d79ac8183febcf7a22dd5cbc0
Vicky-E <victoriaemmanuel630@gmail.com>
2023-05-01 10:35:47 -0700

    comb

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -13,11 +13,11 @@ int z;
 
 while (y <= 9)
 {
-	for (z = x + 1; z <= 9; z++;)
+	for (z = x + 1; z <= 9; z++)
 	{
-		for (x = y + 1; x <= 9; x++;)
+		for (x = y + 1; x <= 9; x++)
 		{
-                	if (x == y == z)
+                	if (x == y && y == z)
 				continue;
 			putchar('0' + y);
 			putchar('0' + x);
@@ -30,7 +30,7 @@ while (y <= 9)
 		}
 		z++;
 	}
-	y++
+	y++;
 }
 	putchar('\n');
 	return (0);
