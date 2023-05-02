96ba87e92c0ae8afb51b0fa00dd27e0bc4837c47
Vicky-E <victoriaemmanuel630@gmail.com>
2023-05-02 03:04:39 -0700

    24

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -4,9 +4,10 @@
  * jack_bauer - prints every hour and minute of the day
  * Return: none
  */
-void jack_bauer()
+void jack_bauer(void)
 {
 	int h, m;
+
 	for (h = 0; h < 24; h++)
 	{
 		for (m = 0; m < 60; m++)
