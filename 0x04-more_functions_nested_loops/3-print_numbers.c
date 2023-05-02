058f83be15854121d010029912b094b4ff8d2944
Vicky-E <victoriaemmanuel630@gmail.com>
2023-05-02 05:18:22 -0700

    numbers

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,15 @@
+#include <unistd.h>
+#include "main.h"
+/**
+ * print_numbers - prints from 0 to 9
+ *
+ * Return: none
+ */
+void print_numbers(void)
+{
+	int num;
+
+	for (num = 0; num <= 9; num++)
+		_putchar(a);
+	_putchar('\n');
+}
