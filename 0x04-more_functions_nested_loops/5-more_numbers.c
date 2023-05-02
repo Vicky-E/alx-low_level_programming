4c524d878656c0a81d2b2c97f171dfe96b8834e4
Vicky-E <victoriaemmanuel630@gmail.com>
2023-05-02 08:42:25 -0700

    more

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -7,21 +7,21 @@
 void more_numbers(void)
 {
 	
-int x;
-int y;
+	int x;
+	int y;
 
-for (y = 0; y < 11; y++)
-{
-	for (x = 0; x <= 14; x++)
+	for (y = 0; y < 11; y++)
 	{
-		if (x > 9)
+		for (x = 0; x <= 14; x++)
 		{
-			_putchar((x / 10) + '0');
-			_putchar((x % 10) + '0');
+			if (x > 9)
+			{
+				_putchar((x / 10) + '0');
+				_putchar((x % 10) + '0');
+			}
+			else
+				_putchar(x + '0');
 		}
-		else
-			_putchar(x + '0');
+		_putchar('\n');
 	}
-	_putchar('\n');
-}
 }
