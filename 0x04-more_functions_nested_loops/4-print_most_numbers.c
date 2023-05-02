e18c39f5313ca19f1c90180c2763ea95766b8c04
Vicky-E <victoriaemmanuel630@gmail.com>
2023-05-02 06:30:44 -0700

    most numbers

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -7,14 +7,13 @@
  */
 void print_most_numbers(void)
 {
-	int n = 0;
+	int n;
 
-	while (n <= 9)
+	for (n = 0; n <= 9; n++)
 	{
 		if (n == 2 || n == 4)
 			continue;
 		_putchar(n + '0');
-		n++;
 	}
 	_putchar('\n');
 }
