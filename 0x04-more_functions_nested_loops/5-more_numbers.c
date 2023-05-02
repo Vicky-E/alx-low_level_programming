3202ad4826f0b2e56303bf422e06e4643c4d95fc
Vicky-E <victoriaemmanuel630@gmail.com>
2023-05-02 06:44:44 -0700

    more

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,25 @@
+#include <unistd.h>
+#include "main.h"
+/**
+ * more_numbers - prints multiple numbers
+ *
+ */
+void more_numbers(void)
+{
+	
+int x;
+int y;
+for (y = 0; y < 11; y++)
+{
+	for (x = 0; x <= 14; x++)
+	{
+		if (x > 9)
+		{
+			_putchar((x / 10) + '0');
+			_putchar((x % 10) + '0');
+		}
+		else
+			_putchar(x + '0');
+	}
+	_putchar('\n');
+}
