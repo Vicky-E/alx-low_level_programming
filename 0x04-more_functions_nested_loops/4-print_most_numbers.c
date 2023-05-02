0ac1b44f34a3dc633104940ce7192a2df91a7d0a
Vicky-E <victoriaemmanuel630@gmail.com>
2023-05-02 05:59:34 -0700

    most

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,19 @@
+#include <unistd.h>
+#include "main"
+/**
+ * print_most_numbers - prints most numbers
+ *
+ * Return - none
+ */
+void print_most_numbers (void)
+{	int n;
+
+	while (n <= 9)
+	{
+		if (n == 2 || n == 4)
+			continue;
+		_putchar(n);
+		n++;
+	}
+	_putchar('\n');
+}
