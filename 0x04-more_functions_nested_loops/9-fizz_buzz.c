75930f181f90842993e94109121c1fe726a39363
Vicky-E <victoriaemmanuel630@gmail.com>
2023-05-04 13:48:07 -0700

    one to hundred

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -7,6 +7,7 @@
 int main(void)
 {
 	int n;
+	
 	for (n = 1; n <= 100; n++)
 	{
 		if ((n % 3) == 0)
@@ -16,8 +17,10 @@ int main(void)
 		else if (((n % 3) == 0) && ((n % 5) == 0))
 			printf("fizzbuzz ");
 		else
+		{
 			printf("%d", n);
 			putchar(' ');
+		}
 	}
 	printf("\n");
 	return (0);
