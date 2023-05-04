967170b8c6890fe028b179ac9af64949e1bd185b
Vicky-E <victoriaemmanuel630@gmail.com>
2023-05-04 13:43:22 -0700

    fizz

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+/**
+ * main - prints 1-100
+ * Return: 0
+ */
+int main(void)
+{
+	int n;
+	for (n = 1; n <= 100; n++)
+	{
+		if ((n % 3) == 0)
+			printf("fizz ");
+		else if ((n % 5) == 0)
+			printf("buzz ");
+		else if (((n % 3) == 0) && ((n % 5) == 0))
+			printf("fizzbuzz ");
+		else
+			printf("%d", n);
+			putchar(' ');
+	}
+	printf("\n");
+	return (0);
+}
