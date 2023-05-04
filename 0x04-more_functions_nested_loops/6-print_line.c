2d131503b1a6ed94e35aa6dce8ba65442784ae39
Vicky-E <victoriaemmanuel630@gmail.com>
2023-05-04 01:42:15 -0700

    print lines

--- 0x04-more_functions_nested_loops/6-print_line.c
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -9,10 +9,13 @@ void print_line(int n)
 {
 	while (n)		 
 	{
-		if (n <= 0)
+		if (n > 0)
+		{
+			_putchar('_');
+			n++;
+		}
+		else
 			break;
-		_putchar('_');
-		n++;
 	}
 	_putchar('\n');
 }
