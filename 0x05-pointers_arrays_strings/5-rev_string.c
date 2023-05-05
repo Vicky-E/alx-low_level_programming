3dd3477a0fcdbd7227003137d9838bd3ad1dc5f9
Vicky-E <victoriaemmanuel630@gmail.com>
2023-05-05 15:29:59 -0700

    string

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -2,6 +2,7 @@
 #include "main.h"
 /**
  * rev_string - reverses a string
+ * @s: string to be reversed
  * Return: nothing
  */
 void rev_string(char *s)
@@ -15,6 +16,6 @@ void rev_string(char *s)
 	}
 	for (i = l - 1; i >= 0; i--)
 	{
-		s[i];
+		s = s[i];
 	}
 }
