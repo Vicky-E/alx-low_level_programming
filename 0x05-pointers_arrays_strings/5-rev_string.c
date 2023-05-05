da735c8668e3cfb3bbc1cf92c814a3b3fd698287
Vicky-E <victoriaemmanuel630@gmail.com>
2023-05-05 15:46:13 -0700

    string

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -7,15 +7,15 @@
  */
 void rev_string(char *s)
 {
-	int l;
+	int len = 0;
 	int i;
 
 	while (s[i++])
 	{
-		l++;
+		len++;
 	}
-	for (i = l - 1; i >= 0; i--)
+	for (i = len - 1; i >= 0; i--)
 	{
-		*s = s[i];
+		s = s[i];
 	}
 }
