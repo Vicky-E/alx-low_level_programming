36d8a6faa33c93206c57d4bbc414568360e65cbc
Vicky-E <victoriaemmanuel630@gmail.com>
2023-05-05 17:02:27 -0700

    times

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -26,15 +26,9 @@ void times_table(void)
 				_putchar(',');
 				_putchar(' ');
 			}
-			/*Uelse if (z == 8 || z == 9)
-			{
-				_putchar(z + '0');
-				_putchar(',');
-				_putchar(' ');
-			}*/
 			else
 			{
-				_putchar((z % 10) + '0');
+				_putchar(z + '0');
 				if (x == 9)
 					break;
 				_putchar(',');
