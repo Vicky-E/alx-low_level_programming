d707e02d5cb99db21c31c91023ca2f7a383baaf6
Vicky-E <victoriaemmanuel630@gmail.com>
2023-05-06 02:33:08 -0700

    reverse

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -8,14 +8,14 @@
 void print_rev(char *s)
 {
 	int l = 0;
-	int i;
+	int i = 0;
 
 	while (s[i++])
 		l++;
 	for (i = l - 1; i >= 0; i--)
 	{
 		if (i == 0)
-			continue;
+			break;
 	}
 		_putchar(s[i]);
 	_putchar('\n');
