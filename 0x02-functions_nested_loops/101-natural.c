eb85cdf15a248ed690bf68abafd335ee1ffbea5a
Vicky-E <victoriaemmanuel630@gmail.com>
2023-05-07 04:38:37 -0700

    natural numbers

---
+++ 0x02-functions_nested_loops/101-natural.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+ * main - sums up multiples of 3 or 5
+ * Return: 0
+ */
+int main(void)
+{
+	int n;
+	int i = 0;
+	
+	for(n = 1; n < 1024; n++)
+	{
+		if (n % 3 == 0 || n % 5 == 0)
+			i += n;
+	}
+	printf("%d\n", i);
+	return (0);
+}
