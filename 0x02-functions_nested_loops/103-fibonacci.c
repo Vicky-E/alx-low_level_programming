3432f2fabe6024496e18694cafb4a83e44e6a810
Vicky-E <victoriaemmanuel630@gmail.com>
2023-05-07 07:05:35 -0700

    fibonacci sum

---
+++ 0x02-functions_nested_loops/103-fibonacci.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+/**
+ * main - sums up even fibonacci numbers below 4000000
+ * Return: 0
+ */
+int main(void)
+{
+	unsigned long int u, w, x, y, z, a;
+
+	a = 0;
+	w = 1;
+	x = 2;
+
+	for (y = 0; y < 48; y++)
+	{
+		z = w + x;
+		if (z % 2 == 0)
+		{
+			if (z >= 4000000)
+				break;
+			a += z;
+		}
+		u = x;
+		w = u;
+		x = z;
+	}
+	printf("%lu\n", a);
+}
