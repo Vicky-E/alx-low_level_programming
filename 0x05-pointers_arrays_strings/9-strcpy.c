b8598e09d8d4caa06e18fe58eea12fade6cd8716
Vicky-E <victoriaemmanuel630@gmail.com>
2023-05-09 15:12:56 -0700

    copies string

---
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,18 @@
+#include <unistd.h>
+#include "main.h"
+/**
+ * _strcpy - copies a string
+ * @dest: string to be copied
+ * @src: string
+ * Return: nothing
+ */
+char *_strcpy(char *dest, char *src)
+{
+	int i = 0;
+	while (src[i])
+	{
+		*dest = *src;
+		return (*dest);
+		i++;
+	}
+}
