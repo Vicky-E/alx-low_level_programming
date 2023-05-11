b4a613510644629a7ac17a32cd53aabca914b9c2
Vicky-E <victoriaemmanuel630@gmail.com>
2023-05-11 05:02:38 -0700

    strcat

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,33 @@
+#include <unistd.h>
+#include "main.h"
+/**
+ * _strcat - appends one string to another
+ * @dest: string to be appended to
+ * @src: string to be appended
+ * Return: nothing
+ */
+char *_strcat(char *dest, char *src)
+{
+	int i = 0, l = 0, L = 0;
+	int len;
+
+	while (dest[i])
+	{
+		i++;
+		l++;
+	}
+	while (src[i])
+	{
+		i++;
+		L++;
+	}
+	len = l + L;
+	for (i = 0; i < len; i++)
+	{
+		if (i < l)
+			dest[len] = dest[i];
+		else
+		dest[len] = src[i];
+	}
+	return (dest);
+}
