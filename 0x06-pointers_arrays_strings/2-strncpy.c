219f768b61accebe5a60df2bb61a595346db1496
Vicky-E <victoriaemmanuel630@gmail.com>
2023-05-12 03:44:50 -0700

    strncpy

---
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -0,0 +1,25 @@
+#include <unistd.h>
+#include "main.h"
+/**
+ * _strncpy - copies string upto n byte
+ * @dest: string to be copied to
+ * @src:string to be copied
+ * @n: number of bytes
+ * Return: pointer to dest
+ */
+char *_strncpy(char *dest, char *src, int n)
+{
+	int i = 0;
+        int l = 0;
+
+        while (src[i] != '\0')
+        {
+		i++;
+        }
+        for (l = 0; l != '\0'; l++,i++)
+	{
+		if (i == n)
+			break;
+		dest[l] = src[i];
+	}
+        return (dest);
