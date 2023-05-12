a0b5d275f18e0d1f69b1e2f812d9342d4eb3193d
Vicky-E <victoriaemmanuel630@gmail.com>
2023-05-12 05:53:40 -0700

    copies

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -11,11 +11,13 @@ char *_strncpy(char *dest, char *src, int n)
 {
 	int i;
 
-	for (i = 0; src[i] != n; i++)
+	for (i = 0; i < n && src[i] != '\0'; i++)
 	{
-		if (i == n)
-			break;
 		dest[i] = src[i];
+	}
+	for ( ; i < n; i++)
+	{
+		dest[i] = '\0';
 	}
 	return (dest);
 }
