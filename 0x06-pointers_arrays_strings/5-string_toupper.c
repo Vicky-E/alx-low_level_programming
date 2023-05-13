7f1807b3070b598278d6c81575aab4214d0c73be
Vicky-E <victoriaemmanuel630@gmail.com>
2023-05-13 14:22:49 -0700

    capitalizes strings

---
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -0,0 +1,18 @@
+#include <unistd.h>
+#include "main.h"
+/**
+ * string_toupper - changes all lowercase to uppercase
+ * @n: string to be changed
+ * Return: n
+ */
+char *string_toupper(char *n)
+{
+	int i;
+
+	for (i = 0; n[i] != '\0'; i++)
+	{
+		if (n[i] >= 'a' && n[i] < 'z')
+			n[i] = n[i] - 32;
+	}
+	return (n);
+}
