fe4bb14316816bb9bd3fc8798cdecdbad4fdacb2
Vicky-E <victoriaemmanuel630@gmail.com>
2023-05-14 07:24:49 -0700

    checks for a character

---
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,20 @@
+#include <unistd.h>
+#include "main.h"
+/**
+ * _strchr - checks for a character in a string
+ * @s: string
+ * @c: character
+ * Return: s
+ */
+char *_strchr(char *s, char c)
+{
+	int i;
+
+	for (i = 0; s[i] != '\0'; i++)
+	{
+		if (s[i] == 'c')
+			return (s);
+		else
+			return (NULL);
+	}
+}
