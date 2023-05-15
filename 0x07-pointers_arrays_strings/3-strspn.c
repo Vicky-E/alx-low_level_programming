74e3bb692381689ac384e14d1d684aaf6512e557
Vicky-E <victoriaemmanuel630@gmail.com>
2023-05-15 16:55:22 -0700

    many

--- 0x07-pointers_arrays_strings/3-strspn.c
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -18,7 +18,7 @@ unsigned int _strspn(char *s, char *accept)
 	}
 	for (i = 0; s[i] != '\0'; i++)
 	{
-		if (s[i] == ',' || s[i) == ' ')
+		if (s[i] == ',' || s[i] == ' ')
 			break;
 		for (j = 0; accept[j] != '\0'; j++)
 		{
