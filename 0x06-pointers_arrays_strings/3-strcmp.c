5bc5513def986a8a9774bffc4f3a4e9208cfbdfa
Vicky-E <victoriaemmanuel630@gmail.com>
2023-05-16 16:26:25 -0700

    compares twostrings

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -11,17 +11,20 @@ int _strcmp(char *s1, char *s2)
 	int i, j;
 	int a;
 
-	for (i = 0; s1[i] != '\0'; i++)
-	{
-		for (j = 0; s2[j] != '\0'; j++)
+	for (i = 0, j = 0; s1[i] != '\0' &&  s2[j] != '\0'; i++, j++)
 		{
 			if (s1[i] == s2[j])
 				a = 0;
 			else if (s1[i] < s2[j])
+			{
 				a = -1;
+				break;
+			}
 			else if (s1[i] > s2[j])
+			{
 				a = 1;
+				break;
+			}
 		}
-	}
 	return (a);
 }
