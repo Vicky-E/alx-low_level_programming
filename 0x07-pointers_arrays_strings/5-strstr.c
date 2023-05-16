34e4a17ddf600baf49dab153a2847de0d619e284
Vicky-E <victoriaemmanuel630@gmail.com>
2023-05-16 03:03:23 -0700

    searches for a substring

---
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -0,0 +1,22 @@
+#include <unistd.h>
+#include "main.h"
+/**
+ * _strstr - locates a substring
+ * @haystack: string
+ * @needle: substring
+ * Return: pointer to haystack or NULL
+ */
+char *_strstr(char *haystack, char *needle)
+{
+	int i = 0;
+
+	while (*haystack)
+	{
+		if (*haystack == *needle)
+		{
+			return (haystack);
+		}
+		haystack++;
+	}
+	return (NULL);
+}
