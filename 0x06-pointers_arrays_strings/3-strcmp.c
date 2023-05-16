e841819605c4dbce424114a0c9f0bdd94cc46f3c
Vicky-E <victoriaemmanuel630@gmail.com>
2023-05-16 16:07:02 -0700

    compares 2 stringa

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -11,14 +11,17 @@ int _strcmp(char *s1, char *s2)
 	int i;
 	int a;
 
-	for (i = 0; s1[i] != '\0' && s2[i] != '\0'; i++)
+	for (i = 0; s1[i] != '\0'; i++)
 	{
-		if (s2 == s1)
-			a = 0;
-		else if (s2 < s1)
-			a = -1;
-		else
-			a = 1;
+		for (j = 0; s2[j] != '\0'; j++)
+		{
+			if (s1[i] == s2[j])
+				a = 0;
+			else if (s1[i] < s2[j])
+				a = -1;
+			else if (s1[i] > s2[j])
+				a = 1;
+		}
 	}
 	return (a);
 }
