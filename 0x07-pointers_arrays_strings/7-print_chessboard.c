a83477929dd5fa794899625a197a60ce01dab6ac
Vicky-E <victoriaemmanuel630@gmail.com>
2023-05-22 04:29:34 -0700

    prints the chessboard

--- 0x07-pointers_arrays_strings/7-print_chessboard.c
+++ 0x07-pointers_arrays_strings/7-print_chessboard.c
@@ -13,15 +13,8 @@ void print_chessboard(char (*a)[8])
 	{
 		for (j = 0; j < 8; j++)
 		{
-			if (i <= 1)
-				_putchar(a[i][j]);
-			else if (i > 1 && i <= 8 - 3)
-				_putchar(' ');
-			else if (i > 8 - 3)
 				_putchar(a[i][j]);
 		}
-		if (i == 8 - 1)
-			break;
 		_putchar('\n');
 	}
 }
