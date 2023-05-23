635ef091f33a850f60f25044b6d06137bfd125af
Vicky-E <victoriaemmanuel630@gmail.com>
2023-05-23 04:36:26 -0700

    multiplies two integers

---
+++ 0x0A-argc_argv/3-mul.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+/**
+ * main - multiplies two numbers
+ * @a: integer
+ * @b: integer
+ * Return: 0 on sucess
+ */
+int main(int a, int b)
+{
+	int argc, c;
+
+	if (argc < 2)
+	{
+		printf("%s\n Error");
+		return (1);
+	}
+	c = a * b;
+	printf("%d\n", c);
+	return (0);
+}
