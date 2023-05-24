0b18cf78bfd4705919efc7e22c4be57dbc1e5b1f
Vicky-E <victoriaemmanuel630@gmail.com>
2023-05-23 19:12:56 -0700

    prints argument count

--- 0x0A-argc_argv/1-args.c
+++ 0x0A-argc_argv/1-args.c
@@ -6,14 +6,8 @@
  * @argv: vector
  * Return: 0 on sucess
  */
-int main(int argc, char *argv[])
+int main(int argc, char __attribute__ ((unused)) *argv[])
 {
-	int i;
-
-	for (i = 0; i < argc; i++)
-	{
-		argv[i];
-	}
 	printf("%d\n", argc);
 	return (0);
 }
