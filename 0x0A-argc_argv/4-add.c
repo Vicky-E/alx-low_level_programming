7a46e6056744339e2a44d9764256c991693e9808
Vicky-E <victoriaemmanuel630@gmail.com>
2023-05-23 19:52:20 -0700

    adds two numbers

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -23,7 +23,7 @@ int main(int argc, char *argv[])
 				printf("Error\n");
 				return (1);
 			}
-			else if (atoi(argv[i]) >= '0')
+			else if (atoi(argv[i]) >= 0)
 			{
 				sum = sum + atoi(argv[i]);
 			}
