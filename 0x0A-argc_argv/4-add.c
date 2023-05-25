906602097abf8dbbeac7bc6be50283e93148da75
Vicky-E <victoriaemmanuel630@gmail.com>
2023-05-24 21:44:36 -0700

    adds positive numbers

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -10,25 +10,23 @@ int main(int argc, char *argv[])
 {
 	int i, sum = 0;
 
-	if (argc <= 0)
+	if (argc == 0)
 	{
 		printf("0\n");
+		return (0);
 	}
-	else
+	for (i = 1; i < argc; i++)
 	{
-		for (i = 1; i < argc; i++)
+		if (atoi(argv[i]) >= 0)
 		{
-			if (atoi(argv[i]) >= 0)
-			{
-				sum = sum + atoi(argv[i]);
-			}
-			else
-			{
-				printf("Error\n");
-				return (1);
-			}
+			sum = sum + atoi(argv[i]);
 		}
-		printf("%d\n", sum);
+		else
+		{
+			printf("Error\n");
+			return (1);
+		}
 	}
+	printf("%d\n", sum);
 	return (0);
 }
