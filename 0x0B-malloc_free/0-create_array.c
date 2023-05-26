0a333c79bd5ec1d60b3878f8818e1bd25a5675f4
Vicky-E <victoriaemmanuel630@gmail.com>
2023-05-26 10:36:49 -0700

    creates an array

---
+++ 0x0B-malloc_free/0-create_array.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include "main.h"
+/**
+ * create_array - creates an array of character
+ * size: size of array
+ * @c: character
+ * Return: character
+ */
+char *create_array(unsigned int size, char c)
+{
+	char *s;
+
+	if (size == 0)
+		return ('\0');
+	s[size];
+	s[0] = 'c';
+	return (s);
+}
---
+++ 0x0B-malloc_free/main.h
@@ -0,0 +1,5 @@
+/**
+ * function prototypes
+ */
+int _putchar(char c);
+char *create_array(unsigned int size, char c);
