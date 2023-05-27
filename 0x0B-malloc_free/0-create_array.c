e3293e7ff6b6ef2efe8a2dcb5cef43097adea48c
Vicky-E <victoriaemmanuel630@gmail.com>
2023-05-27 08:43:51 -0700

    creates arrays

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -11,14 +11,14 @@
 char *create_array(unsigned int size, char c)
 {
 	char *s;
-	int i;
+	unsigned int i;
 
 	if (size <= 0)
 		return ('\0');
 	for (i = 0; i < size;)
 	{
 		if (i == 0)
-			s[i] = 'c';
+			s[i] = c;
 		s[i];
 		i++;
 	}
