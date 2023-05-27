d5e05328dae9363bc9d8a0ee2d2d36a906ad783c
Vicky-E <victoriaemmanuel630@gmail.com>
2023-05-27 11:42:24 -0700

    copies a string into newly allocated memory

---
+++ 0x0B-malloc_free/1-strdup.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "main.h"
+/**
+ * _strdup - returns a pointer to a newly allocated memory space
+ * @str: string to be copied
+ * Return: pointer or null
+ */
+char *_strdup(char *str)
+{
+	char *copy;
+
+	copy = malloc(sizeof(str));
+
+	if (copy == NULL || str == NULL)
+		return ('\0');
+	copy = str;
+	return (copy);
+}
