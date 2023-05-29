b7f53fbe4c1e7cece7af9a58e83c2b8707f16daf
Vicky-E <victoriaemmanuel630@gmail.com>
2023-05-29 02:42:22 -0700

    allocates concatenated string new memory

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -10,16 +10,33 @@
  */
 char *str_concat(char *s1, char *s2)
 {
-	int length, i, ls1, ls2;
-	char *strcat;
+	int length, i, ls1, ls2, j = 0, k = 0;
+	char *strcat, *n, *m;
 
 	if (s1 == NULL && s2 == NULL)
 		return (NULL);
 	if (s1 == NULL && s2 != NULL)
-		return (malloc(strlen(s2)));
+	{
+		n = malloc(strlen(s2));
+		while (*s2)
+		{
+			n[j] = *s2;
+			j++;
+			s2++;
+			return (n);
+		}
+	}
 	if (s2 == NULL && s1 != NULL)
-		return (malloc(strlen(s1)));
-
+	{
+		m = malloc(strlen(s1));
+		while (*s1)
+		{
+			m[k] = *s1;
+			k++;
+			s1++;
+			return (m);
+		}
+	}
 	ls1 = strlen(s1);
 	ls2 = strlen(s2);
 	length = ls1 + ls2 + 1;
