a718105bc74e207581092cba095291e5187f808e
Vicky-E <victoriaemmanuel630@gmail.com>
2023-05-29 02:22:59 -0700

    concatenates strings

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -16,9 +16,9 @@ char *str_concat(char *s1, char *s2)
 	if (s1 == NULL && s2 == NULL)
 		return (NULL);
 	if (s1 == NULL && s2 != NULL)
-		return (s2);
+		return (malloc(strlen(s2)));
 	if (s2 == NULL && s1 != NULL)
-		return (s1);
+		return (malloc(strlen(s1)));
 
 	ls1 = strlen(s1);
 	ls2 = strlen(s2);
