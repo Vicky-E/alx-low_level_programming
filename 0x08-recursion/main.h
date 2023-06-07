6e4b9a23a36155998acd187cbfc4d0f874bf4f91
victoria emmanuel <victoriaemmanuel630@gmail.com>
2023-06-07 15:51:45 +0100

    Delete 0x08-recursion directory

--- 0x08-recursion/main.h
+++
@@ -1,9 +0,0 @@
-/**
- * function prototypes
- */
-int _putchar(char c);
-void _puts_recursion(char *s);
-int _strlen_recursion(char *s);
-int factorial(int n);
-int _pow_recursion(int x, int y);
-void _print_rev_recursion(char *s);
