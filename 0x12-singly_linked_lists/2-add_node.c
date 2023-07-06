0b3919956caabc770458d3ba91a9877ee0693317
Vicky-E <victoriaemmanuel630@gmail.com>
2023-07-06 15:16:55 -0700

    creates new node

---
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -0,0 +1,30 @@
+#include "lists.h"
+/**
+ * add_node - adds new node at start
+ * @head: head
+ * @str: elements
+ * Return: pointer
+ */
+list_t *add_node(list_t **head, const char *str)
+{
+	char *s;
+	int l;
+	list_t *node;
+
+	node = malloc(sizeof(list_t));
+	if (node == NULL)
+		return (NULL);
+	s = strdup(str);
+	if (s == NULL)
+	{
+		free(node);
+		return (NULL);
+	}
+	for (l = 0; str[l];)
+		l++;
+	node->str = s;
+	node->len = l;
+	node->next = *head;
+	*head = node;
+	return(node);
+}
