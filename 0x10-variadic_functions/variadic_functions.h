b39ebf6bbf9dbd397ef9d7ed0c5a20d86d90f792
Vicky-E <victoriaemmanuel630@gmail.com>
2023-07-06 08:47:58 +0100

    deleted stray data

--- 0x10-variadic_functions/variadic_functions.h
+++ 0x10-variadic_functions/variadic_functions.h
@@ -2,14 +2,12 @@
 #define VARIADIC_FUNCTIONS_H
 int _putchar(char c);
 int sum_them_all(const unsigned int n, ...);
-<<<<<<< HEAD
 typedef struct prints
 {
 	char *sym;
 	void(*print)(va_list args);
 }printer;
-=======
+
 void print_numbers(const char *separator, const unsigned int n, ...);
 void print_strings(const char *separator, const unsigned int n, ...);
 #endif
->>>>>>> 1841bd1a90aac3870c123eaf186ce6271a4cdeaa
