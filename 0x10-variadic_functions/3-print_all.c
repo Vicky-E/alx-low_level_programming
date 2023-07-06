4462eb328218dd9a0cc1572c7afc246e5e2414c7
Vicky-E <victoriaemmanuel630@gmail.com>
2023-07-05 21:03:23 -0700

    prints for different formats

---
+++ 0x10-variadic_functions/3-print_all.c
@@ -0,0 +1,73 @@
+#include <stdarg.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include "variadic_functions.h"
+/**
+ * print_all - prints for various specifiers
+ * @format: character string
+ */
+void pchar(va_list args);
+void pstr(va_list args);
+void pint(va_list args);
+void pfloat(va_list args);
+void print_all(const char * const format, ...);
+
+void pchar(va_list args)
+{
+	char ch;
+
+	ch = va_arg(args, int);
+	printf("%c", ch);
+}
+void pstr(va_list args)
+{
+	char *str;
+
+	str = va_arg(args, char *);
+	if (str == NULL)
+		str = "(nil)";
+	printf("%s", str);
+}
+void pint(va_list args)
+{
+	int num;
+
+	num = va_arg(args, int);
+	printf("%d", num);
+}
+void pfloat(va_list args)
+{
+	float dec;
+
+	dec = va_arg(args, double);
+	printf("%f", dec);
+}
+void print_all(const char * const format, ...)
+{
+	va_list args;
+	int x = 0, y = 0;
+	char *s = "";
+	printer func[] = {
+
+		{"c", pchar},
+		{"s", pstr},
+		{"i", pint},
+		{"f", pfloat}
+	};
+	va_start(args, format);
+	while (format && (*(format + y)))
+	{
+		x = 0;
+		while (x < 4 && ((*(format + y)) != *func[x].sym))
+			x++;
+		if (x < 4)
+		{
+			printf("%s", s);
+			func[x].print(args);
+			s = ", ";
+		}
+		y++;
+	}
+	printf("\n");
+}
+
--- 0x10-variadic_functions/variadic_functions.h
+++ 0x10-variadic_functions/variadic_functions.h
@@ -1,2 +1,7 @@
 int _putchar(char c);
 int sum_them_all(const unsigned int n, ...);
+typedef struct prints
+{
+	char *sym;
+	void(*print)(va_list args);
+}printer;
