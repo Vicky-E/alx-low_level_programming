a9deeddf0bf073d288f1a3a09382204acd5da97d
Vicky-E <victoriaemmanuel630@gmail.com>
2023-07-06 04:19:11 -0700

    changed return value

--- 0x0F-function_pointers/2-int_index.c
+++ 0x0F-function_pointers/2-int_index.c
@@ -11,16 +11,16 @@ int int_index(int *array, int size, int (*cmp)(int))
 {
 	int i = 0;
 
-	if (size <= 0 || array == NULL)
+	if (cmp == NULL || array == NULL)
 		return (-1);
 	while (size > 0)
 	{
 		cmp(*array);
 		if (cmp != 0)
-			break;
+			return (i);
 		array++;
 		i++;
 		size--;
 	}
-	return (i);
+	return (-1);
 }
