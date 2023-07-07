49b7745a576c6d2e493a570d9fffccf185a39687
Vicky-E <victoriaemmanuel630@gmail.com>
2023-07-07 12:52:14 -0700

    added description for function

--- 0x13-more_singly_linked_lists/5-free_listint2.c
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -1,7 +1,7 @@
 #include "lists.h"
 #include <stdio.h>
 /**
- * free_listint - frees list_int
+ * free_listint2 - frees list_int
  * @head: pointer to list
  */
 void free_listint2(listint_t **head)
