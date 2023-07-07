2665c49dd2b8c66d9408957992dc691f85d40699
Vicky-E <victoriaemmanuel630@gmail.com>
2023-07-07 20:31:58 +0100

    removed dereferencing

--- 0x13-more_singly_linked_lists/5-free_listint2.c
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -8,7 +8,7 @@ void free_listint2(listint_t **head)
 {
 	listint_t *new;
 
-	if (*head == NULL)
+	if (head == NULL)
 		return;
 
 	while (*head)
@@ -17,5 +17,5 @@ void free_listint2(listint_t **head)
 		free(*head);
 		*head = new;
 	}
-	*head = NULL;
+	head = NULL;
 }
