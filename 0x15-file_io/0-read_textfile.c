35d5e7e345211b5aa94fe3a3cff92484d14cae8d
Vicky-E <victoriaemmanuel630@gmail.com>
2023-07-10 22:29:10 -0700

    saving all files

---
+++ 0x15-file_io/0-read_textfile.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * read_textfile - reads a textfile
+ * @filename: name of file to be read
+ * @letters: number of letters it should print
+ * Return: actual number of letters it could print and read
+ */
+ssize_t read_textfile(const char *filename, size_t letters)
+{
+	size_t count;
+	FILE *f;
+
+	if (filename == NULL)
+		return (0);
+	f = fopen(filename, "r");
+	if (f == NULL)
+		return (0);
+
+
