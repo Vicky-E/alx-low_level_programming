df7fc9411297f68e633a3e4cac0bf3c22914d156
Vicky-E <victoriaemmanuel630@gmail.com>
2023-07-18 11:13:57 -0700

    opening,reading and printing content of file to posix output

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -7,13 +7,21 @@
  */
 ssize_t read_textfile(const char *filename, size_t letters)
 {
-	size_t count;
+	ssize_t count;
 	FILE *f;
+	char str[letters];
 
 	if (filename == NULL)
 		return (0);
 	f = fopen(filename, "r");
 	if (f == NULL)
 		return (0);
-
-
+	do {
+		str[letters] = fgetc(f);
+		if (feof(f))
+			break;
+		write(STDOUT_FILENO, &str, 1);
+		count++;
+	} while (1);
+	return (count);
+}
--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -2,7 +2,7 @@
 #define MAIN_H
 #include <unistd.h>
 #include <stdio.h>
-#include <sys/types.h>
+#include <fcntl.h>
 #include <stdlib.h>
 int _putchar(char c);
 ssize_t read_textfile(const char *filename, size_t letters);
