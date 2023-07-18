95ff6eb88fb045fca23d407509243719b011c15f
Vicky-E <victoriaemmanuel630@gmail.com>
2023-07-18 11:49:48 -0700

    used system call

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -7,20 +7,23 @@
  */
 ssize_t read_textfile(const char *filename, size_t letters)
 {
-	ssize_t count;
-	FILE *f;
-	char str;
+	ssize_t f, a, w;
+	char *str;
 
 	if (filename == NULL)
 		return (0);
-	f = fopen(filename, "r");
-	if (f == NULL)
+	str = malloc(sizeof(char) * letters);
+	if (str == NULL)
 		return (0);
-	do {
-		str = fgetc(f);
-		write(STDOUT_FILENO, &str, 1);
-		count++;
-	} while (str != EOF);
-	fclose(f);
-	return (count);
+	f = open(filename, O_RDONLY);
+	a = read(f, str, letters);
+	w = write(STDOUT_FILENO, str, a);
+	if (f == -1 || a == -1 || w == -1 || w != a)
+	{
+		free(str);
+		return (0);
+	}
+	free(str);
+	close(f);
+	return (w);
 }
