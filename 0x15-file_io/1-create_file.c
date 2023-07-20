c270c1e6bed33d90acb8a6a2afa53eaf6da9342e
Vicky-E <victoriaemmanuel630@gmail.com>
2023-07-20 14:04:25 -0700

    added ermissions

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -13,7 +13,7 @@ int create_file(const char *filename, char *text_content)
 	if (filename == NULL)
 		return (-1);
 	len = strlen(text_content);
-	c = open(filename, O_RDWR | O_CREAT | O_TRUNC);
+	c = open(filename, O_RDWR | O_CREAT | O_TRUNC, 0600);
 	if (c == -1)
 		return (-1);
 	if (text_content != NULL)
