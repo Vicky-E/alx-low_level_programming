22e8b0c4f01a1451de596b5ff019c70720c81923
Vicky-E <victoriaemmanuel630@gmail.com>
2023-07-20 14:56:01 -0700

    added new variable for length

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -7,20 +7,15 @@
  */
 int append_text_to_file(const char *filename, char *text_content)
 {
-	int o, w;
+	int o, w, len = 0;
 
 	if (filename == NULL)
 		return (-1);
 	if (text_content != NULL)
-	{
-		o = open(filename, O_RDWR || O_APPEND);
-		w = write(o, text_content, strlen(text_content));
-		if (o == -1 || w == -1)
-			return (-1);
-		return (1);
-	}
-	o = open(filename, O_RDWR);
-	if (o == -1)
+		len = strlen(text_content);
+	o = open(filename, O_WRONLY || O_APPEND);
+	w = write(o, text_content, len);
+	if (o == -1 || w == -1)
 		return (-1);
 	close(o);
 	return (1);
