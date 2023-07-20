dc1a4a2441b50ff265afa1b8c215ab7da1aeda2b
Vicky-E <victoriaemmanuel630@gmail.com>
2023-07-20 13:57:26 -0700

    changed file permission

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -14,7 +14,7 @@ int create_file(const char *filename, char *text_content)
 		return (-1);
 	len = strlen(text_content);
 	c = open(filename, O_RDWR | O_CREAT | O_TRUNC);
-	chmmod 600 c;
+	chmod(filename, 600);
 	if (c == -1)
 		return (-1);
 	if (text_content != NULL)
