314e976a669103341f294dc765885bdd495cfb95
Vicky-E <victoriaemmanuel630@gmail.com>
2023-07-20 14:01:12 -0700

    removed permits

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -13,7 +13,6 @@ int create_file(const char *filename, char *text_content)
 	if (filename == NULL)
 		return (-1);
 	len = strlen(text_content);
-	chmod(filename, S_IRWXU);
 	c = open(filename, O_RDWR | O_CREAT | O_TRUNC);
 	if (c == -1)
 		return (-1);
