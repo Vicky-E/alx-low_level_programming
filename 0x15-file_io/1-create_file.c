18bf047de91299dcd68f1c985f23758c13aac81e
Vicky-E <victoriaemmanuel630@gmail.com>
2023-07-20 13:42:21 -0700

    changed if condition

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -19,8 +19,6 @@ int create_file(const char *filename, char *text_content)
 		return (-1);
 	if (text_content != NULL)
 		w = write(c, text_content, len);
-	else
-		w = write(c, NULL, len);
 	if (w == -1)
 	{
 		return (-1);
