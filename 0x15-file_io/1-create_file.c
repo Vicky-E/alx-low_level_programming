fdc8277c7baaf20fa8445afd281268f1372a9b54
Vicky-E <victoriaemmanuel630@gmail.com>
2023-07-20 14:13:54 -0700

    added if condition

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -8,11 +8,12 @@
  */
 int create_file(const char *filename, char *text_content)
 {
-	int c, w, len;
+	int c, w, len = 0;
 
 	if (filename == NULL)
 		return (-1);
-	len = strlen(text_content);
+	if (text_content != NULL)
+		len = strlen(text_content);
 	c = open(filename, O_CREAT | O_RDWR | O_TRUNC, 0600);
 	if (c == -1)
 		return (-1);
