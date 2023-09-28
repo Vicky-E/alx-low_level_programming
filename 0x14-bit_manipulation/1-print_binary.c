fa119c6111369069fc30d255f85860a5beb82b68
Vicky-E <victoriaemmanuel630@gmail.com>
2023-09-28 14:39:57 -0700

    prints bit equivalent given index

---
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -0,0 +1,9 @@
+#include "main.h"
+#include <unistd.h>
+#include <string.h>
+/**
+ * print_binary - prints binary equivalent of n
+ * @n: int to be converted
+ */
+void print_binary(unsigned long int n)
+{
---
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -0,0 +1,18 @@
+#include "main.h"
+#include <unistd.h>
+#include <stdio.h>
+/**
+ * get_bit - returns bit value given index
+ * @n: integer with bit value
+ * @index: position of return bit
+ * Return: value at index
+ */
+int get_bit(unsigned long int n, unsigned int index)
+{
+	int bitlen;
+
+	bitlen = sizeof(n) * 8;
+	if (index < 0 || index >= bitlen)
+		return (-1);
+	return ((n >> index) & 1);
+}
