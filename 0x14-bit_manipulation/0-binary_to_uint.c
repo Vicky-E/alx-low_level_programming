61c5e6e2f8c547be59e17e1571a1d92d9eaeb38e
Vicky-E <victoriaemmanuel630@gmail.com>
2023-09-28 09:19:53 -0700

    fixed styling error

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -8,22 +8,22 @@
  */
 unsigned int binary_to_uint(const char *b)
 {
-	int mul,len, i, conv, index = 0;
+	int mul, len, i, conv, index = 0;
 	unsigned int uint = 0;
 
 	len = strlen(b);
 	i = len - 1;
 	if (b == NULL)
-		return(0);
+		return (0);
 	while (b[index] != NULL && i >= 0)
 	{
 		if  ((b[index] != '0') && (b[index] != '1'))
-			return(0);
+			return (0);
 		conv = b[index] - '0';
 		mul = conv << i;
 		uint += mul;
 		index++;
 		i--;
 	}
-	return(uint);
+	return (uint);
 }
