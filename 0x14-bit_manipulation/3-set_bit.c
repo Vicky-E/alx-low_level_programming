a1c17c2b6e9da9179aa96c632dc011dbbc586d69
Vicky-E <victoriaemmanuel630@gmail.com>
2023-09-28 15:40:16 -0700

    added descriptions

--- 0x14-bit_manipulation/3-set_bit.c
+++ 0x14-bit_manipulation/3-set_bit.c
@@ -2,18 +2,18 @@
 #include <unistd.h>
 #include <string.h>
 /**
- *
- *
- *
+ * set_bit - sets indeed value to 1
+ * @n: integer with index
+ * @index: position of value
  */
 int set_bit(unsigned long int *n, unsigned int index)
 {
 	unsigned int bitlen, bit;
 
-	bitlen = sizeof(n) * 8;
+	bitlen = (sizeof(n)) * 8;
 	if (index >= bitlen)
 		return (-1);
-	bit = (*n >> index);
+	bit = (*n >> index) & 1;
 	bit = bit | 1;
 	return (1);
 }
