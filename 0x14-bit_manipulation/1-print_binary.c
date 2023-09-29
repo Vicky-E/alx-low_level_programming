59076460e78b9971f5fefe7c5261923a69f41640
Vicky-E <victoriaemmanuel630@gmail.com>
2023-09-28 21:23:09 -0700

    prints binary equivalent of int

--- 0x14-bit_manipulation/1-print_binary.c
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -1,9 +1,20 @@
 #include "main.h"
 #include <unistd.h>
-#include <string.h>
+#include <stdlib.h>
 /**
  * print_binary - prints binary equivalent of n
  * @n: int to be converted
  */
 void print_binary(unsigned long int n)
 {
+	int i, bin, bitlen;
+	bitlen = (sizeof(n)) * 8;
+	for (i = bitlen - 1; i >=0; i--)
+	{
+		bin = n >> i;
+		if (bin & 1)
+			_putchar('1');
+		else
+			_putchar('0');
+	}
+}
