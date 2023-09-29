83c396b93d6ddf244df3183b423c2154811891f5
Vicky-E <victoriaemmanuel630@gmail.com>
2023-09-29 04:07:56 -0700

    removed leading bits

--- 0x14-bit_manipulation/1-print_binary.c
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -7,14 +7,17 @@
  */
 void print_binary(unsigned long int n)
 {
-	int i, bin, bitlen;
+	int i, bin, bitlen, sigbit = 0;
 	bitlen = (sizeof(n)) * 8;
-	for (i = bitlen - 1; i >=0; i--)
+	for (i = bitlen - 1; i >= 0; i--)
 	{
 		bin = n >> i;
 		if (bin & 1)
+		{
 			_putchar('1');
-		else
+			sigbit = 1;
+		}
+		else if (sigbit)
 			_putchar('0');
 	}
 }
