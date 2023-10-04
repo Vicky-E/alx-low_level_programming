bbc8f8ad8cf1ccaaf5e78a684c95c54f5015b679
Vicky-E <victoriaemmanuel630@gmail.com>
2023-10-04 09:00:56 -0700

    truncates file

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -17,11 +17,11 @@ int create_file(const char *filename, char *text_content)
 		close(create_file);
 		return (1);
 	}
-	create_file = open(filename, O_CREAT | O_WRONLY |O_TRUNC, 0600);
+	create_file = open(filename, O_CREAT | O_TRUNC | O_WRONLY, 0600);
 	if (create_file == -1)
 		return (-1);
-	write(create_file, &text_content, sizeof(text_content));
-	if ((write(create_file, &text_content, sizeof(text_content))) == -1)
+	write(create_file, &text_content, strlen(text_content));
+	if ((write(create_file, &text_content, strlen(text_content))) == -1)
 		return (-1);
 	close(create_file);
 	return (1);
