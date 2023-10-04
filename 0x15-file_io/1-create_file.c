ab4c52166dd4f45b644aeb2ffe766b7726184cf3
Vicky-E <victoriaemmanuel630@gmail.com>
2023-10-04 09:03:46 -0700

    removed adress

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -20,8 +20,8 @@ int create_file(const char *filename, char *text_content)
 	create_file = open(filename, O_CREAT | O_TRUNC | O_WRONLY, 0600);
 	if (create_file == -1)
 		return (-1);
-	write(create_file, &text_content, strlen(text_content));
-	if ((write(create_file, &text_content, strlen(text_content))) == -1)
+	write(create_file, text_content, strlen(text_content));
+	if ((write(create_file, text_content, strlen(text_content))) == -1)
 		return (-1);
 	close(create_file);
 	return (1);
