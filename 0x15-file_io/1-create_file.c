2d04615b7209c936cc8ac0c406bcf0e37e069f31
Vicky-E <victoriaemmanuel630@gmail.com>
2023-10-04 14:57:38 -0700

    debugging

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -7,21 +7,20 @@
  */
 int create_file(const char *filename, char *text_content)
 {
-	int create_file;
+	int create_file, bytes;;
 
 	if (filename == NULL)
 		return (-1);
 	if (text_content == NULL)
 	{
-		create_file = open(filename, O_CREAT);
-		close(create_file);
-		return (1);
+		text_content = " ";
 	}
+	bytes = (strlen(text_content) + 1);
 	create_file = open(filename, O_CREAT | O_TRUNC | O_WRONLY, 0600);
 	if (create_file == -1)
 		return (-1);
-	write(create_file, text_content, strlen(text_content));
-	if ((write(create_file, text_content, strlen(text_content))) == -1)
+	write(create_file, text_content, bytes);
+	if ((write(create_file, text_content, bytes)) == -1)
 		return (-1);
 	close(create_file);
 	return (1);
