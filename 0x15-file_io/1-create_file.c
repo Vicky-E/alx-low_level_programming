c47a1649c828026ce9c23211c06018cae7a13c7e
Vicky-E <victoriaemmanuel630@gmail.com>
2023-10-04 15:01:19 -0700

    found size of string

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -15,7 +15,10 @@ int create_file(const char *filename, char *text_content)
 	{
 		text_content = " ";
 	}
-	bytes = (strlen(text_content) + 1);
+	while (*text_content != '\0')
+	{
+		bytes++;
+	}
 	create_file = open(filename, O_CREAT | O_TRUNC | O_WRONLY, 0600);
 	if (create_file == -1)
 		return (-1);
