e077e575daf0dde33d63e5c3e067d2e68910a7a7
Vicky-E <victoriaemmanuel630@gmail.com>
2023-10-05 13:13:19 -0700

    checked return value of write call

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -7,7 +7,7 @@
  */
 int create_file(const char *filename, char *text_content)
 {
-	int create_file, bytes = 0;
+	int create_file, fd, bytes = 0;
 
 	if (filename == NULL)
 		return (-1);
@@ -22,8 +22,8 @@ int create_file(const char *filename, char *text_content)
 	create_file = open(filename, O_CREAT | O_TRUNC | O_WRONLY, 0600);
 	if (create_file == -1)
 		return (-1);
-	write(create_file, text_content, bytes);
-	if ((write(create_file, text_content, bytes)) == -1)
+	fd = write(create_file, text_content, bytes);
+	if (fd == -1)
 		return (-1);
 	close(create_file);
 	return (1);
