45e5a86a5e9d791ba890a723fc09f7b2aa7a3cef
Vicky-E <victoriaemmanuel630@gmail.com>
2023-10-06 01:35:11 -0700

    assigned number of bytes

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -33,6 +33,7 @@ int main(int argc, char *argv[])
 	}
 	file_from = open(argv[1], O_RDONLY);
 	file_to = open(argv[2], O_CREAT | O_WRONLY | O_TRUNC, 0664);
+	file_read = 1024;
 	while (file_read == 1024)
 	{
 		file_read = read(file_from, buffer, 1024);
